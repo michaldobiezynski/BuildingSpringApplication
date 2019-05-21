e23efce088157b519dc4f2ea564f073c982dd0e3
Michal Dobiezynski <michal.dobiezynski@woodmac.com>
2019-05-21 10:46:10 +0100

    commit before break

--- src/com/ps/springm1/MyBankApp.java
+++ src/com/ps/springm1/MyBankApp.java
@@ -2,13 +2,18 @@ package com.ps.springm1;
 
 import com.ps.springm1.model.CurrentAccount;
 import com.ps.springm1.model.SavingAccount;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyBankApp {
     public static void main(String []args) {
 
         //SavingAccount account = new SavingAccount();
-       // Account account = new SavingAccount();
-        Account account = new CurrentAccount();
+        //Account account = new SavingAccount();
+        //Account account = new CurrentAccount();
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("resources/applicationContext.xml");
+
+        Account account = context.getBean("myAccount", Account.class);
         System.out.println(account.createAccount());
     }
 }
