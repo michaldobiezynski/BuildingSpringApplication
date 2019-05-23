fd25b5c683ed74faa08c4f75cc951a52f6e724a8
Michal Dobiezynski <michal.dobiezynski@woodmac.com>
2019-05-23 11:34:13 +0100

    spring bean singleton scope

--- src/com/ps/springm1/MyBankApp.java
+++ src/com/ps/springm1/MyBankApp.java
@@ -13,8 +13,20 @@ public class MyBankApp {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Account account = context.getBean("myAccount", Account.class);
-        System.out.println(account.createAccount());
-        System.out.println(account.cardDetails());
+        Account account1 = context.getBean("myAccount", Account.class);
+        Account account2 = context.getBean("myAccount", Account.class);
+
+        boolean isSame = (account1 == account2);
+
+        System.out.println("Account 1 and Account 2 pointing to the same object " + isSame);
+        System.out.println("Account 1 Hash: " + account1.hashCode());
+        System.out.println("Account 2 Hash: " + account2.hashCode());
+
+
+//        System.out.println(account.createAccount());
+//        System.out.println(account.cardDetails());
+
+
+
     }
 }
